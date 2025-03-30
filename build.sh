722edd8897dbb977655bfd05a35a3e3b263a2361
qwertzer12 <qwertzer12@outlook.com>
2025-03-30 22:37:50 +0100

    Improve build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,22 @@ sudo apt update && sudo apt upgrade -y
 
 # Install necessary packages
 echo "Installing required packages..."
-sudo apt install -y python3 python3-venv python3-pip
+sudo apt install -y python3 python3-venv python3-pip gcc
+
+# Go install
+echo "Installing Go"
+wget https://go.dev/dl/go1.24.1.linux-arm64.tar.gz && sudo tar -C /usr/local -xzf go1.24.1.linux-arm64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+sudo rm -rf go1.21.1.linux-arm64.tar.gz
+
+# Lazygit install
+echo "Installing Lazygit"
+git clone https://github.com/jesseduffield/lazygit.git
+cd lazygit
+go build -o lazygit
+sudo mv lazygit /usr/local/bin/
+cd ..
+rm -rf lazygit/
 
 # Create and activate virtual environment
 echo "Setting up Python virtual environment..."
@@ -19,7 +34,7 @@ python3 -m venv .venv
 echo "Activating virtual environment..."
 source .venv/bin/activate
 
-# Install dependencies
+# Install dependencies wq
 if [ -f "requirements.txt" ]; then
   echo "Installing dependencies from requirements.txt..."
   pip install -r requirements.txt
